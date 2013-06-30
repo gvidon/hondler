ac51b89a85887fd9542450305af7fab722d0da87
nide <nenegoro@gmail.com>
2013-06-30 04:02:45 +0400

    This will make installing hondler package by pip possible

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+setup(
+	name             = 'hondler',
+	version          = '0.0.1',
+	description      = 'Django cart and checkout + frontend js',
+	long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+	author           = 'Artem Rudenko',
+	author_email     = 'nenegoro@gmail.com',
+	url              = 'https://github.com/gvidon/hondler/',
+	packages         = ['hondler'],
+)
